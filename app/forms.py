14d3108268b09477c7cbac4d589d1fd20017643f
unknown <2926232264@qq.com>
2022-09-07 12:52:50 +0800

    9.7.2022 添加user，todolist，event模型

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField
 from wtforms.validators import DataRequired
 
+
 class CalculatorForm(FlaskForm):
     number1 = IntegerField('number1', validators=[DataRequired()])
     number2 = IntegerField('number2', validators=[DataRequired()])
--- app/models.py
+++ app/models.py
@@ -3,8 +3,35 @@ from app import db
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), index=True, unique=True)
-    password = db.Column(db.String(20), password=True)
+    password = db.Column(db.String(20))
     admin = db.Column(db.Boolean, default=False)
-    rigister_date = db.Column(db.DateTime)
-    last_login_time = db.Column(db.DateTime)
+    register_datetime = db.Column(db.DateTime)
+    last_login_datetime = db.Column(db.DateTime)
 
+    def __repr__(self) -> str:
+        return '<User % r>' % (self.id, self.username, self.admin)
+
+
+class TodoListModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
+    list_name = db.Column(db.String(20), index=True, unique=True)
+    limit = db.Column(db.Integer)
+    properties = db.relationship('Property', backref='todo_list', lazy='dynamic')
+
+    def __repr__(self) -> str:
+        return '<TodoList % r>' % (self.id, self.user_id, self.list_name)
+
+
+class EventModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    todo_list_id = db.Column(db.Integer, db.ForeignKey('todo_list.id'), unique=True)
+    title = db.Column(db.String(20), index=True)
+    content = db.Column(db.Text(100))
+    create_datetime = db.Column(db.DateTime)
+    setting_datetime = db.Column(db.DateTime)
+    finished = db.Column(db.Boolean, default=False)
+    finished_datetime = db.Column(db.DateTime, nullable=True)
+
+    def __repr__(self) -> str:
+        return '<Event % r>' % (self.id, self.todo_list_id, self.title, self.content, self.setting_time, self.finished)
