0d205866d7ef6d76d2a7baebd9ea9bfe98f0b32d
unknown <2926232264@qq.com>
2022-09-07 14:32:58 +0800

    9.7.2022 修改：ORM模型；添加：数据库创建脚本

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # database config
 HOSTNAME = '127.0.0.1'
 PORT = '3306'
-DATABASE = 'mysql'
+DATABASE = 'flask'
 USERNAME = 'root'
 PASSWORD = 'Yxk20021205'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
